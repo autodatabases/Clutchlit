41406a40ab960ded35e3d83d76cec338bf4a5269
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-01-02 13:20:27 +0100

    Dashboard added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,15 +6,35 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Clutchlit.Models;
 using Microsoft.AspNetCore.Authorization;
+using Clutchlit.Data;
 
 namespace Clutchlit.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        
+        private readonly MysqlContext _contextSp24;
+        private readonly AMysqlContext _contextSp;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context, MysqlContext contextSp24, AMysqlContext contextSp)
+        {
+            _contextSp24 = contextSp24;
+            _contextSp = contextSp;
+            _context = context;
+        }
         public IActionResult Index()
         {
+            int numberSp24 = _contextSp24.Orders_sp24.Where(p => p.Current_state == 1 || p.Current_state == 3 || p.Current_state == 16).Count();
+            int numberSpcom = _contextSp.Orders_spcom.Where(p => p.Current_state == 1 || p.Current_state == 3 || p.Current_state == 16).Count();
+
+            int disPrices = _context.PdPrices.Count();
+
+            int customers = _contextSp24.Customers_sp24.Count() + _contextSp.Customers_spcom.Count();
+
+            ViewData["newOrders"] = numberSp24 + numberSpcom;
+            ViewData["distributorsPrices"] = disPrices;
+            ViewData["customerAmount"] = customers;
             return View();
         }
 
