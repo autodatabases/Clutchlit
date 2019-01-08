f59847a87a33def35171aa9c29c31fd92d65c29e
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-01-08 09:06:08 +0100

    Allegro auction adding v5

---
+++ Models/DeliveryMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Clutchlit.Models
+{
+    public class DeliveryMethod
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
