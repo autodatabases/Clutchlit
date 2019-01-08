ab21ec757fce85d82dd32a9f8536ce8e4188492c
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-01-08 10:53:18 +0100

    Allegro auction - category select

--- wwwroot/js/custom/allegro.js
+++ wwwroot/js/custom/allegro.js
@@ -15,4 +15,49 @@
         })
     });
 
+    $("#category2").change(function () {
+        $('#category3').find('option').remove().end().append('<option  selected=""> - Wybierz kateogrię - </option>').val('kategoria');
+        var url = '/AllegroAuctions/GetChildCategories';
+        var ddlsource = "#mark";
+
+        $.getJSON(url, { parent_id: $('#maincategories').val() }, function (data) {
+            var items = ' ';
+            $("#category3").empty();
+            $.each(data, function (i, row) {
+                items += "<option value='" + row.value + "'>" + row.text + "</option>";
+            });
+            $('#category3').html(items);
+        })
+    });
+
+    $("#category3").change(function () {
+        $('#category4').find('option').remove().end().append('<option  selected=""> - Wybierz kateogrię - </option>').val('kategoria');
+        var url = '/AllegroAuctions/GetChildCategories';
+        var ddlsource = "#mark";
+
+        $.getJSON(url, { parent_id: $('#maincategories').val() }, function (data) {
+            var items = ' ';
+            $("#category4").empty();
+            $.each(data, function (i, row) {
+                items += "<option value='" + row.value + "'>" + row.text + "</option>";
+            });
+            $('#category4').html(items);
+        })
+    });
+
+    $("#category5").change(function () {
+        $('#category5').find('option').remove().end().append('<option  selected=""> - Wybierz kateogrię - </option>').val('kategoria');
+        var url = '/AllegroAuctions/GetChildCategories';
+        var ddlsource = "#mark";
+
+        $.getJSON(url, { parent_id: $('#maincategories').val() }, function (data) {
+            var items = ' ';
+            $("#category5").empty();
+            $.each(data, function (i, row) {
+                items += "<option value='" + row.value + "'>" + row.text + "</option>";
+            });
+            $('#category5').html(items);
+        })
+    });
+
 });
