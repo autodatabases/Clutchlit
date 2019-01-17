1699c43adea4708a06f6f83bfce1b755f6722c2a
Mikołaj Ferfet <m.ferfet@ponowemu.pl>
2019-01-17 08:59:50 +0100

    Photo uploading - Allegro part 2

--- wwwroot/js/custom/allegro.js
+++ wwwroot/js/custom/allegro.js
@@ -89,27 +89,29 @@
     // pobieramy parametry dla kategorii
 
     // wysyłamy zdjęcia na serwer
-    $("#imageUploadForm").change(function() {
-         var formData = new FormData();
-         var totalFiles = document.getElementById("imageUploadForm").files.length;
-         for (var i = 0; i < totalFiles; i++) {
-           var file = document.getElementById("imageUploadForm").files[i];
-           formData.append("imageUploadForm", file);
-         }
-        $.ajax({
-            type: "POST",
-            url: '/AllegroAuctions/UploadPhotos',
-            data: formData,
-            dataType: 'json',
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                alert('succes!!');
-            },
-            //error: function(error) {
-            //    alert("errror");
-            //}
-        });
-    });   
+    $("#confirmPhotos").on('click', function (e) {
+        
+            var formData = new FormData();
+            var totalFiles = $("#imageUploadForm").files.length;
+            for (var i = 0; i < totalFiles; i++) {
+                var file = $("#imageUploadForm").files[i];
+                formData.append("imageUploadForm", file);
+            }
+            $.ajax({
+                type: "POST",
+                url: '/AllegroAuctions/UploadPhotos',
+                data: formData,
+                dataType: 'json',
+                contentType: false,
+                processData: false,
+                success: function (response) {
+                    alert('succes!!');
+                },
+                //error: function(error) {
+                //    alert("errror");
+                //}
+            });   
+    });
+    
 
 });
